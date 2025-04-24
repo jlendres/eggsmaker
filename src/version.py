34ec6cef4c62d958c9fac6d9ca19e76d59517bee
Jorge lLuis Endres <jlecomputer04@gmail.com>
2025-04-24 09:19:45 -0300

    Merge branch 'main' of https://github.com/pieroproietti/eggsmaker

--- src/version.py
+++ src/version.py
@@ -1,3 +1,11 @@
 #!/bin/bash
+<<<<<<< HEAD
 VERSION="0.0.6"
 APP="eggsmaker"
+=======
+version="0.0.5"
+app="Eggs Maker"
+
+__version__ = version
+__app__ = app
+>>>>>>> b6502cf9fbddce746ae159aaa78f95df44c15a0a
